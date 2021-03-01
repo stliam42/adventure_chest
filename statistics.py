21232559767a0f5633ea4a931aab6221cf639a96
alexkarachun42@gmail.com <alexkarachun42@gmail.com>
2021-03-01 22:46:20 +1000

    treasures

--- statistics.py
+++ statistics.py
@@ -2,7 +2,13 @@ class Stats():
     """Settings class for AC game"""
 
     def __init__(self):
+        """Initialize game statistics"""
+        # Dungeon
         self.dungeon_level = 0
         self.trip_number = 1
-        self.exp = 0
         self.dragon_awake = False
+
+        # Hero
+        self.exp = 0
+        self.ability_used = False
+        
