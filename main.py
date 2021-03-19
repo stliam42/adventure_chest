6b9934ceab56f3606150afb29708f4a6df81be0b
Alex <alexk@DESKTOP-HD74KLR>
2021-03-19 13:44:48 +1000

    Слияние в ветку мастер.

--- main.py
+++ main.py
@@ -2,23 +2,9 @@ from adventure_chest import AdventureChest
 
 
 ac_game = AdventureChest()
-#ac_game.game_procces_cycle()
+ac_game.game_procces_cycle()
 
 
-
-
-print(ac_game.hero.ability_check())
-ac_game.hero.get_exp(5)
-
-ac_game.dungeon = ["Гоблин", "Сундук", "Зелье"]*3
-ac_game.party.create(3)
-ac_game.treasures.get_treasure(5)
-
-ac_game._print_party_info()
-
-ac_game.hero.ability()
-
-ac_game._print_party_info()
 
 
 
